14af6a858a6127b5f7c76d0eef4ab99ae265bb54
Okuhle charlieman <okuhlecharlieman72@gmail.com>
2025-02-18 20:30:24 +0200

    Frame added

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
+import classes.AppFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       new AppFrame();
     }
 }
--- src/classes/AppFrame.java
+++ src/classes/AppFrame.java
@@ -1,5 +1,15 @@
 package classes;
+import javax.swing.JFrame;
 
-public class AppFrame {
-
+public class AppFrame extends JFrame {
+    //Attributes
+ private TitleBar titleBar;
+    private List list;
+    private ButtonPanel buttonPanel;
+//Constructor
+    public AppFrame() {
+      this.setSize(400, 700);
+      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      this.setVisible(true);
+    }
 }
