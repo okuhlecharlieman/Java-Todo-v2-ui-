b75c35b2e7806cc5c246920e66020306e36040cf
Okuhle charlieman <okuhlecharlieman72@gmail.com>
2025-03-19 22:01:24 +0200

    added a text to title bar

--- src/classes/TitleBar.java
+++ src/classes/TitleBar.java
@@ -1,7 +1,9 @@
 package classes;
 
 import java.awt.Color;
-import java.awt.Dimension;  
+import java.awt.Dimension;
+import java.awt.Font;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class TitleBar extends JPanel {
@@ -9,6 +11,9 @@ public class TitleBar extends JPanel {
     public TitleBar() {
        this.setPreferredSize(new Dimension(400, 80));
        this.setBackground(Color.DARK_GRAY);
+       JLabel titletext = new JLabel("To-Do List");
+         titletext.setFont(new Font("Arial", Font.BOLD, 24));
+         this.add(titletext); 
     }
 
 }
