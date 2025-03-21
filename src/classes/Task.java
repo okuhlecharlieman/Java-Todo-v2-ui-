e9230a9259aaaa97792caa19cfea5558c9f9c895
Okuhle charlieman <okuhlecharlieman72@gmail.com>
2025-03-21 21:00:20 +0200

    added task

--- src/classes/Task.java
+++ src/classes/Task.java
@@ -1,5 +1,21 @@
 package classes;
 
-public class Task {
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Task extends JPanel {
+    private JLabel index;
+    private JTextField taskName;
+    private JButton done;
+    private boolean checked;
+
+    //Constructor
+    Task(){
+        this.setPreferredSize(new Dimension(40,20));
+    }
 
 }
