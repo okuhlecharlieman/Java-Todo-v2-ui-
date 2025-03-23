e349adb325a624d9ec8a27dc147383b6ecbcc9e8
Okuhle charlieman <okuhlecharlieman72@gmail.com>
2025-03-23 22:18:58 +0200

    added add task functionality

--- src/classes/AppFrame.java
+++ src/classes/AppFrame.java
@@ -1,12 +1,17 @@
 package classes;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import java.awt.BorderLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class AppFrame extends JFrame {
     //Attributes
  private TitleBar title;
     private List list;
     private ButtonPanel btnPanel;
+    private JButton addTask;
+    private JButton clear;
 //Constructor
     public AppFrame() {
       this.setSize(600, 700);
@@ -16,9 +21,23 @@ public class AppFrame extends JFrame {
       title = new TitleBar();
       list = new List();
       btnPanel = new ButtonPanel();
+      addTask = btnPanel.getAddBtn();
+      clear = btnPanel.getRemoveBtn();
       this.add(title, BorderLayout.NORTH);
       this.add(btnPanel, BorderLayout.SOUTH);
       this.add(list, BorderLayout.CENTER);
       this.setVisible(true);
+
+      addListeners();
+    }
+    private void addListeners() {
+      addTask.addMouseListener(new MouseAdapter() {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+          Task task = new Task();
+          list.add(task);
+         revalidate();
+        }
+      });
     }
 }
