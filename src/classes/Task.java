cebe7ce075853427050dd6f3b0d2c9fc48db9005
Okuhle charlieman <okuhlecharlieman72@gmail.com>
2025-03-23 13:03:23 +0200

    added taskName

--- src/classes/Task.java
+++ src/classes/Task.java
@@ -4,10 +4,12 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 public class Task extends JPanel {
     private JLabel index;
@@ -26,6 +28,11 @@ index = new JLabel("");
 index.setPreferredSize(new Dimension(20, 20));
 index.setHorizontalAlignment(JLabel.CENTER);
 this.add(index, BorderLayout.WEST);
+
+taskName = new JTextField("Your task here");
+taskName.setBorder(BorderFactory.createEmptyBorder());
+taskName.setBackground(Color.red);
+this.add(taskName, BorderLayout.CENTER);
     }
 
 }
